c142d713525edd07066b4f55b22db9064a6f7bf4
Subharup Nandi <165703131+subhwastaken@users.noreply.github.com>
2026-08-03 01:18:22 +0530

    LC-valid-anagram [Runtime: N/A; Memory: N/A]

--- sorting/valid-anagram.py
+++ sorting/valid-anagram.py
@@ -1,27 +1,24 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        hashmap = {}
 
         if len(s) != len(t):
             return False
-
-        hashmap = {}
+        
         for i in s:
             if i in hashmap:
                 hashmap[i] += 1
             else:
                 hashmap[i] = 1
-        for j in t:
-            if j in hashmap:
-                hashmap[j] -= 1
+        
+        for i in t:
+            if i in hashmap:
+                hashmap[i] -= 1
             else:
-                return False
-                
-            if hashmap[j] < 0:
+                hashmap[i] = 1
+
+    
+        for val in hashmap.values():
+            if val != 0:
                 return False
         return True
-
-        # other way is
-        # for values in hashmap.values():
-        #     if values != 0:
-        #         return False
-        # return True
