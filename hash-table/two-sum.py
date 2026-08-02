1c0ea8777b2281cc24cda56719a700d4bdddb0fe
Subharup Nandi <165703131+subhwastaken@users.noreply.github.com>
2026-08-03 00:56:18 +0530

    LC-two-sum [Runtime: N/A; Memory: N/A]

--- hash-table/two-sum.py
+++ hash-table/two-sum.py
@@ -1,11 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
-        hashmap = {}
-
+        hashmap = {}    
         for i in range(len(nums)):
-            need = target - nums[i]
+            need = target - nums[i] 
             if need in hashmap:
                 return [hashmap[need],i]
-            hashmap[nums[i]] = i
-        
+            hashmap[nums[i]] = i  
