42365ba8d3be7f9bfbde2ed70de4c24654b891ca
Subharup Nandi <165703131+subhwastaken@users.noreply.github.com>
2026-08-03 18:24:43 +0530

    LC-group-anagrams [Runtime: 54 ms; Memory: 21.8 MB]

--- array/group-anagrams.py
+++ array/group-anagrams.py
@@ -1,16 +1,18 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    
         hashmap = {}
         ans = []
+    
         for word in strs:
             key = "".join(sorted(word))
             if key in hashmap:
                 hashmap[key].append(word)
             else:
                 hashmap[key] = [word]
-        
+
+
         for val in hashmap.values():
             ans.append(val)
-        return ans
-    
         
+        return ans
