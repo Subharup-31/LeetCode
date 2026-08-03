225aa734969783afe212aa63fe781768b36ef8b0
Subharup Nandi <165703131+subhwastaken@users.noreply.github.com>
2026-08-03 20:30:02 +0530

    LC-valid-palindrome [Runtime: N/A; Memory: N/A]

--- string/valid-palindrome.py
+++ string/valid-palindrome.py
@@ -1,7 +1,24 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s.lower()
-        clean = "".join(c.lower() for c in s if c in "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789abcdefghijklmnopqrstuvwxyz1234567890")
-        print(clean)
-        return clean == clean[::-1]
+        s = s.lower()
+        i = 0
+        j = len(s) - 1
+
+        while i < j:
+            if not s[i].isalnum():
+                i += 1
+                continue
+                
+            if not s[j].isalnum():
+                j-=1
+                continue
+
+            
+            if s[i] == s[j]:
+                i = i + 1
+                j = j - 1
+            else:
+                return False
+        return True
+
 
