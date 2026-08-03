587d2eec1f216471918fe08d052ef842cc63d98f
Subharup Nandi <165703131+subhwastaken@users.noreply.github.com>
2026-08-03 20:31:53 +0530

    LC-valid-palindrome [Runtime: 9 ms; Memory: 19.7 MB]

--- string/valid-palindrome.py
+++ string/valid-palindrome.py
@@ -1,24 +1,23 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+
+        # the issue with this solution is its taking a extra space where i can direct accept the char which is alphnum and copare that only
+        
+
         s = s.lower()
+        new_s = ""
+        for ch in s:
+            if ch.isalnum():
+                new_s += ch
+
         i = 0
-        j = len(s) - 1
+        j = len(new_s) - 1
 
         while i < j:
-            if not s[i].isalnum():
+            if new_s[i] == new_s[j]:
                 i += 1
-                continue
-                
-            if not s[j].isalnum():
-                j-=1
-                continue
-
-            
-            if s[i] == s[j]:
-                i = i + 1
-                j = j - 1
+                j -= 1
             else:
                 return False
+
         return True
-
-
