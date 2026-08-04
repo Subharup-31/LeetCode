f19968eac7a44bca0c077481567b4983c6d1fe54
Subharup Nandi <165703131+subhwastaken@users.noreply.github.com>
2026-08-04 11:46:20 +0530

    LC-valid-palindrome [Runtime: N/A; Memory: N/A]

--- two-pointers/valid-palindrome.py
+++ two-pointers/valid-palindrome.py
@@ -1,23 +1,27 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
 
-        # the issue with this solution is its taking a extra space where i can direct accept the char which is alphnum and copare that only
-        
-
         s = s.lower()
-        new_s = ""
-        for ch in s:
-            if ch.isalnum():
-                new_s += ch
-
         i = 0
-        j = len(new_s) - 1
+        j = len(s)-1
 
         while i < j:
-            if new_s[i] == new_s[j]:
+            if s[i].isalnum():
+                pass
+            else:
+                i += 1
+                continue
+
+            if s[j].isalnum():
+                pass
+            else:
+                j -= 1
+                continue
+
+            if s[i] == s[j]:
                 i += 1
                 j -= 1
             else:
                 return False
-
+        
         return True
