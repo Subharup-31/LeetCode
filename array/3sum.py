ad63590d615a5cb580569d5795a137326c177385
Subharup Nandi <165703131+subhwastaken@users.noreply.github.com>
2026-08-06 21:07:10 +0530

    LC-3sum [Runtime: N/A; Memory: N/A]

--- array/3sum.py
+++ array/3sum.py
@@ -1,32 +1,25 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
-        
         nums.sort()
-        res = []
+        ans = []
 
-        for i in range(len(nums)):
-            left = i + 1
-            right = len(nums) - 1
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-            while left < right:
-                if (nums[i] + nums[right] + nums[left]) == 0: 
-
-                    res.append([nums[i], nums[right], nums[left]])
-                    
-                    while left < right and nums[left] == nums[left+1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right-1]:
-                        right -=1
-
-
-                    left += 1
-                    right -=1
-
-                elif (nums[i] + nums[right] + nums[left]) > 0:
-                    right -= 1
-                else:
-                    left += 1
-
-
-        return res 
+        #both i and j shodul be inside the j loop cause when k moves forward everyhting shoudl resets
+        for k in range(len(nums)-2):
+            i = k+1
+            j = len(nums)-1
+            if k != 0:
+                if nums[k] == nums[k-1]:
+                    continue 
+            while i < j :
+                # here j!= k and i!=j and i!=k is not reqired as from starting we are pointing 3 pointer in different possitions so it is always gareenteed
+                current_sum = nums[i]+nums[j]+nums[k]
+                if current_sum == 0 :
+                    ans.append([nums[i],nums[j],nums[k]])    
+                    i += 1
+                    j -= 1
+                elif current_sum < 0:
+                    i += 1
+                elif current_sum > 0:
+                    j -= 1
+        return ans
+                 
