94216c99d933960dc58a9f8e2067400096aa1a14
Subharup Nandi <165703131+subhwastaken@users.noreply.github.com>
2026-08-06 21:24:07 +0530

    LC-3sum [Runtime: N/A; Memory: N/A]

--- array/3sum.py
+++ array/3sum.py
@@ -3,23 +3,35 @@ class Solution:
         nums.sort()
         ans = []
 
-        #both i and j shodul be inside the j loop cause when k moves forward everyhting shoudl resets
-        for k in range(len(nums)-2):
-            i = k+1
-            j = len(nums)-1
+        # both i and j should be inside the loop because every new k axts as a reset to i and j
+        for k in range(len(nums) - 2):
+            i = k + 1
+            j = len(nums) - 1
+
             if k != 0:
-                if nums[k] == nums[k-1]:
-                    continue 
-            while i < j :
-                # here j!= k and i!=j and i!=k is not reqired as from starting we are pointing 3 pointer in different possitions so it is always gareenteed
-                current_sum = nums[i]+nums[j]+nums[k]
-                if current_sum == 0 :
-                    ans.append([nums[i],nums[j],nums[k]])    
+                if nums[k] == nums[k - 1]:
+                    continue
+
+            while i < j:
+                # j!=k i!=j and i!=k are unnecessary because the pointers always start at different indices
+                current_sum = nums[i] + nums[j] + nums[k]
+
+                if current_sum == 0:
+                    ans.append([nums[i], nums[j], nums[k]])
+
                     i += 1
                     j -= 1
+
+                    while i < j and nums[i] == nums[i - 1]:
+                        i += 1
+
+                    while i < j and nums[j] == nums[j + 1]:
+                        j -= 1
+
                 elif current_sum < 0:
                     i += 1
-                elif current_sum > 0:
+
+                else:
                     j -= 1
+
         return ans
-                 
